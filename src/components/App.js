206ccbad8c0ea8d22de64e507f91dffada3e320c
Thilagavathy R <72353262+thilaks-hash@users.noreply.github.com>
2023-06-20 20:49:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,35 @@ import React from 'react';
 import {useState} from 'react';
 import '../styles/App.css';
 const App = () => {
-  const[square,setIsSquare]=useState(true);
- const[shapes,setShapes]=useState([]);
-  const Toggle=()=>{
-    if(square==="Square"){
-    setShapes([...shapes,<div key={shapes.length}className="square">Square</div>])
-    }else if(circle==="Circle"){
-    setShapes([...shapes,<div key={shapes.length}className="circle">Circle</div>])
-  }
-  
-  return (
-    <div id="main">
-      <div id="shape-creator">
-        <select value={square} onChange={(e)=>setIsSquare(e.target.value)}>
-              <option value="Square">Square</option>
-              <option value="Circle">Circle</option>
-        </select>
-    <button className={square?"Square":"Circle"}onClick={Toggle}>Add shape</button>
+const [selectedShape, setSelectedShape] = useState('square');
+  const [shapes, setShapes] = useState([]);
 
+  const handleShapeChange = (event) => {
+    setSelectedShape(event.target.value);
+  };
+
+  const handleAddShape = () => {
+    const newShape = selectedShape === 'square' ? (
+      <div className="square">Square</div>
+    ) : (
+      <div className="circle">Circle</div>
+    );
+
+    setShapes([...shapes, newShape]);
+  };
+
+  return (
+    <div>
+      <div id="shape-creator">
+        <select value={selectedShape} onChange={handleShapeChange}>
+          <option value="square">Square</option>
+          <option value="circle">Circle</option>
+        </select>
+        <button onClick={handleAddShape}>Add shape</button>
       </div>
-      <div id="shapes-holder">{shapes}
-      </div>
+      <div id="shapes-holder">{shapes}</div>
     </div>
-  )
+  );
 }
 
 
