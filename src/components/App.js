df1a985ca3f74101931177df01ff657c9c165de9
Thilagavathy R <72353262+thilaks-hash@users.noreply.github.com>
2023-06-20 15:45:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
 import '../styles/App.css';
 const App = () => {
-
+  const[square,setIsSquare]=useState(true);
+ const[shapes,setShapes]=useState([]);
+  const Toggle=()=>{
+    if(square==="Square"){
+    setShapes([...shapes,<div key={shapes.length}className="square">Square</div>
+    }else{
+    setShapes([...shapes,<div key={shapes.length}className="circle">Circle</div>
+  }
+  
   return (
     <div id="main">
       <div id="shape-creator">
-        <select onChange={handleShapes}>
-              <option>Square</option>
-              <option>Circle</option>
+        <select value={square} onChange={(e)=>setIsSquare(e.target.value)}>
+              <option value="Square">Square</option>
+              <option value="Circle">Circle</option>
         </select>
-    <button className={button}>Add shape</button>
+    <button className={square?"Square":"Circle"}onClick={Toggle}>Add shape</button>
 
       </div>
-      <div id="shapes-holder">
+      <div id="shapes-holder">{shapes}
       </div>
     </div>
   )
